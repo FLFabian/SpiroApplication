2ce7e5d2073a86ba74bbfda3161cc2b0b50fc848
MacOS <MacOS@macbook-pro.local>
2018-08-13 20:51:02 +0200

    Added UI allergy ticker

--- SpiroAppNew/ViewController.swift
+++ SpiroAppNew/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    let allergies = ["Æg", "Mælk", "Fisk", "Gluten", "Soja", "Nødder", "Peanuts", "Kød"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return allergies.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let allergyCells = tableView.dequeueReusableCell(withIdentifier: "allergyCells", for: indexPath)
+        allergyCells.textLabel?.text = allergies[indexPath.row]
+        return allergyCells
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark{
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+            
+        }
+        else {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        
+        }
+    }
+    
+    
 
 }
 
